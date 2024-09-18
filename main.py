88bd643f7cc85550e06122136d6207d45775fc19
turradeira2 <tiagaomatos@hotmail.com>
2024-09-18 15:38:02 +0100

    started working on finding wad files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import platform
 #endregion
 
 #region vars
+wad_location= "test"
 os_name = platform.system()
 print(os_name)
 #endregion
@@ -16,9 +17,16 @@ def on_button_click():
 def show_os():
     messagebox.showinfo("Curret OS",os_name)
 
-# Função para exibir a mensagem "Sobre"
+#region About box
 def show_about():
-    messagebox.showinfo("Sobre", "Este é um exemplo de aplicação Tkinter com menubar.")
+    messagebox.showinfo("About", "This is the About Message Box!")
+#endregion
+
+#region wad folder menu
+def wad_location_select():
+    messagebox.showinfo("WAD LOCATION",wad_location)
+    print(wad_location)
+#endregion
 
 # Função para abrir uma nova janela
 def open_new_window():
@@ -40,16 +48,26 @@ root.geometry("600x400")
 
 #region menubar
 menubar = tk.Menu(root)
+
+#region file menu
 filemenu = tk.Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="WADs Location", command=wad_location_select)
+filemenu.add_separator()
+
 filemenu.add_command(label="Nova Janela", command=open_new_window)
 filemenu.add_separator()
+
 filemenu.add_command(label="Exit", command=quit_app)
 menubar.add_cascade(label="File", menu=filemenu)
+#endregion
 
+#region help menu
 helpmenu = tk.Menu(menubar, tearoff=0)
 helpmenu.add_command(label="About", command=show_about)
 helpmenu.add_command(label="OS", command=show_os)
 menubar.add_cascade(label="Ajuda", menu=helpmenu)
+#endregion
 
 root.config(menu=menubar)
 #endregion
@@ -65,9 +83,17 @@ frame3 = tk.Frame(root, bg="lightcoral", padx=10, pady=10)
 frame3.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
 #endregion
 
+#region WAD frame
+testvar = ('doom','doom 2','sigil','sigil 2')
+wadvar = tk.Variable(value=testvar) 
+
+wadlist = tk.Listbox(wadframe,listvariable=wadvar,height=4,selectmode=tk.EXTENDED )
+wadlist.grid(row=0, column=0,pady=10)
+#endregion
+
 #region widgets
 label1 = tk.Label(wadframe, text="Container 1", bg="lightblue")
-label1.grid(row=0, column=0, pady=10)
+label1.grid(row=2, column=0, pady=10)
 
 button1 = tk.Button(wadframe, text="Botão 1", command=on_button_click)
 button1.grid(row=1, column=0, pady=10)
